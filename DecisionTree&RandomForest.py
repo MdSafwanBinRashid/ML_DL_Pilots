6d36cf5512c71b8c3c8e5cb9c9a594c4cc8205cf
Md Safwan Bin Rashid <mdsafwanbinrashid2@gmail.com>
2025-11-19 17:08:11 -0600

    Update DecisionTree&RandomForest.py

--- DecisionTree&RandomForest.py
+++ DecisionTree&RandomForest.py
@@ -8,22 +8,22 @@ data = load_breast_cancer()
 X = data.data
 y = data.target
 
-x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
 tree_clf = DecisionTreeClassifier()
-tree_clf.fit(x_train, y_train)
+tree_clf.fit(X_train, y_train)
 
 forest_clf = RandomForestClassifier(n_estimators=100)
-forest_clf.fit(x_train, y_train)
+forest_clf.fit(X_train, y_train)
 
-tree_accuracy = tree_clf.score(x_test, y_test)
-forest_accuracy = forest_clf.score(x_test, y_test)
+tree_accuracy = tree_clf.score(X_test, y_test)
+forest_accuracy = forest_clf.score(X_test, y_test)
 
 print(f'Decision Tree Accuracy: {tree_accuracy:.3f}')
 print(f'Random Forest Accuracy: {forest_accuracy:.3f}')
 
-tree_pred = tree_clf.predict([x_test[0]])
-forest_pred = forest_clf.predict([x_test[0]])
+tree_pred = tree_clf.predict([X_test[0]])
+forest_pred = forest_clf.predict([X_test[0]])
 
 print(f"\nTrue Value: {y_test[0]}") # 1 - Benign (non-cancerous), 0 - Malignant (cancerous)
 print(f"Decision Tree Predicted: {'Benign' if tree_pred[0] == 1 else 'Malignant'}")
